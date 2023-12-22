39d57777d82896d3e524d4cf8db748dc2f6a2753
Mihir Arya <msarya@icloud.com>
2023-12-21 18:53:05 -0700

    Updated frontend

---
+++ React/App.tsx
@@ -0,0 +1,36 @@
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-get-random-values';
+import Thread from './screens/Thread';
+import Ingest from './screens/Ingest';
+
+export type RootDrawerParamList = {
+  Thread: { location: string };
+  Ingest: { location: string };
+};
+
+const Drawer = createDrawerNavigator<RootDrawerParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Ingest" component={Ingest} initialParams={{ location: "Mumbai"}} />
+        <Drawer.Screen name="Thread" 
+                      component={Thread} 
+                      initialParams={{ location: "Mumbai"}} 
+                      options={({ route }) => ({title: route.params?.location || "Location" })}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
