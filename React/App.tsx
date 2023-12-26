e5e66984acfdba3fb7763792ee43a4e6331b0f31
Mihir Arya <msarya@icloud.com>
2023-12-26 14:55:50 -0800

    Created individual ingest and query stacks

--- React/App.tsx
+++ React/App.tsx
@@ -1,17 +1,67 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-get-random-values';
+import LocationInput from './components/LocationInput';
 import Thread from './screens/Thread';
 import Ingest from './screens/Ingest';
 
 export type RootDrawerParamList = {
   Thread: { location: string };
   Ingest: { location: string };
+  LocationInput: { process: 'ingest' | 'query' };
 };
 
 const Drawer = createDrawerNavigator<RootDrawerParamList>();
+const Stack = createStackNavigator();
+
+function IngestStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="LocationIngest"
+        component={LocationInput}
+        options={{headerShown: false}}
+        initialParams={{ process: "ingest" }}
+      />
+      <Stack.Screen
+        name="IngestThread"
+        component={Ingest}
+        options={
+          ({route}: { route: { params?: { location?: string } } }) => ({
+            title: route.params?.location,
+            headerBackTitleVisible: false,
+          })
+        }
+      />
+    </Stack.Navigator>
+  );
+}
+
+function QueryStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="LocationQuery"
+        component={LocationInput}
+        options={{headerShown: false}}
+        initialParams={{ process: "query" }}
+      />
+      <Stack.Screen
+          name="QueryThread"
+          component={Thread}
+          options={
+            ({route}: { route: { params?: { location?: string } } }) => ({
+              title: route.params?.location,
+              headerBackTitleVisible: false,
+            })
+          }
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -19,25 +69,23 @@ export default function App() {
       <Drawer.Navigator>
         <Drawer.Screen
           name="Ingest"
-          component={Ingest}
-          initialParams={{ location: "Mumbai" }}
+          component={IngestStack}
           options={{
             title: "Ingest",
-            drawerLabelStyle: { color: "blue", fontWeight: "bold"},
           }}
         />
         <Drawer.Screen
           name="Thread"
-          component={Thread}
-          initialParams={{ location: "Mumbai" }}
-          options={({ route }) => ({
-            title: route.params?.location || "Location",
-          })}
+          component={QueryStack}
+          options={{
+            title: "Query",
+          }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
