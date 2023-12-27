ad9e2c0556e25beaf121d58950f52750e273a6d5
Mihir Arya <msarya@icloud.com>
2023-12-26 17:00:07 -0800

    CORS enabled

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,8 +5,21 @@ from pydantic import BaseModel
 from datetime import datetime
 import utils
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*", # Allow all origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Message(BaseModel):
